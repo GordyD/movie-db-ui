fd3753a0080ea3aa3588922f6d24a12ee22994a8
Gordon Dent <gordonmdent@gmail.com>
2016-08-08 10:24:54 -0700

    Update MovieItem and its styling

--- src/client/components/MovieItem.js
+++ src/client/components/MovieItem.js
@@ -13,27 +13,38 @@ const MovieItem = (props) => {
   let movieInViewContainerClass = cx('MovieInView-container', { 
     visible: (props.movieInView && props.movieInView.id === props.id)
   })
-  let imageElem = <div 
-    className='noImage' 
+  let movieItemImage = <div 
+    className='MovieItem-poster noImage' 
+    onClick={() => props.onShowMovieDetail(props)}>
+    No Image
+  </div>
+  let movieInViewImage = <div 
+    className='MovieInView-poster noImage' 
     onClick={() => props.onShowMovieDetail(props)}>
     No Image
   </div>
   if (props.poster_path) {
-    imageElem = <img 
+    movieItemImage = <img 
+      className='MovieItem-poster'
       alt={props.original_title}
       onClick={() => props.onShowMovieDetail(props)}
       src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${props.poster_path}`} 
     />
+    movieInViewImage = <img 
+      className='MovieInView-poster'
+      alt={props.original_title}
+      src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2/${props.poster_path}`} 
+    />
   }
   return <div className='MovieItem-container'>
-    {imageElem}
-    <h4>{props.original_title} ({moment(props.release_date).format('YYYY')})</h4>
+    {movieItemImage}
+    <h4 className='MovieItem-title'>{props.original_title} ({moment(props.release_date).format('YYYY')})</h4>
     <div className={movieInViewContainerClass} onClick={() => props.onHideMovieDetail()}>
-      {imageElem}
-      <h2>{props.original_title} ({moment(props.release_date).format('YYYY')})</h2>
-      <h3>Rating: <span className='popularity'>{Math.round(props.vote_average)}</span></h3>
-      <h3>Popularity: <span className='popularity'>{Math.round(props.popularity)}</span></h3>
-      <p>{props.overview}</p>
+      {movieInViewImage}
+      <h2 className='MovieInView-title'>{props.original_title} ({moment(props.release_date).format('YYYY')})</h2>
+      <h3 className='MovieInView-rating'>Rating: <span>{Math.round(props.vote_average)}</span></h3>
+      <h3 className='MovieInView-popularity'>Popularity: <span>{Math.round(props.popularity)}</span></h3>
+      <p className='MovieInView-description'>{props.overview}</p>
     </div>
   </div>
 } 
